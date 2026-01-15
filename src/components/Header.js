7d96f3a7132d003e3cd94832832ac5e5a84f82a0
Christian Choi <bob1253456@hotmail.com>
2026-01-16 01:19:53 +0800

    add content

--- src/components/Header.js
+++ src/components/Header.js
@@ -27,21 +27,44 @@ class Header extends Component {
   render() {
     if (this.props.sharedData) {
       var name = this.props.sharedData.name;
-      this.titles = this.props.sharedData.titles.map(x => [ x.toUpperCase(), 1500 ] ).flat();
+      this.titles = this.props.sharedData.titles
+        .map((x) => [x.toUpperCase(), 1500])
+        .flat();
     }
 
-    const HeaderTitleTypeAnimation = React.memo( () => {
-      return <Typical className="title-styles" steps={this.titles} loop={50} />
-    }, (props, prevProp) => true);
+    const HeaderTitleTypeAnimation = React.memo(
+      () => {
+        return (
+          <Typical className="title-styles" steps={this.titles} loop={50} />
+        );
+      },
+      (props, prevProp) => true
+    );
 
     return (
-      <header id="home" style={{ height: window.innerHeight - 140, display: 'block' }}>
-        <div className="row aligner" style={{height: '100%'}}>
+      <header
+        id="home"
+        style={{ height: window.innerHeight - 140, display: "block" }}
+      >
+        <div className="row aligner" style={{ height: "100%" }}>
           <div className="col-md-12">
             <div>
-              <span className="iconify header-icon" data-icon="la:laptop-code" data-inline="false"></span>
-              <br/>
-              <h1 className="mb-0">
+              <span
+                className="iconify header-icon"
+                data-icon="noto:rocket"
+                data-inline="false"
+                style={{ animation: "bounce 2s ease-in-out infinite" }}
+              ></span>
+              <br />
+              <h1
+                className="mb-0"
+                style={{
+                  textShadow:
+                    "2px 2px 4px rgba(0,0,0,0.3), 0 0 20px rgba(255,255,255,0.5)",
+                  fontWeight: "bold",
+                  letterSpacing: "2px",
+                }}
+              >
                 <Typical steps={[name]} wrapper="p" />
               </h1>
               <div className="title-container">
