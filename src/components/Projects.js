bc22bb557f95b5487773527f383bc5adc0ac89fe
Christian Choi <bob1253456@hotmail.com>
2026-01-16 14:51:15 +0800

    Preserve image aspect ratio: use object-fit contain instead of cover

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -31,8 +31,11 @@ class Projects extends Component {
                   <img
                     src={projects.images[0]}
                     alt="projectImages"
-                    height="230"
-                    style={{marginBottom: 0, paddingBottom: 0, position: 'relative'}}
+                    style={{
+                      marginBottom: 0,
+                      paddingBottom: 0,
+                      position: "relative",
+                    }}
                   />
                   <span className="project-date">{projects.startDate}</span>
                   <br />
